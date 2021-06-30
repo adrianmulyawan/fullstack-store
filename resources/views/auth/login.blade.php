52ffc5c911e2ab45f770d3ff52ce27e28a4ef6be
Adrian Mulyawan <42925072+adrianmulyawan@users.noreply.github.com>
2021-06-30 16:44:31 +0700

    Integration Login and edit navbar when login or guest only

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,47 +9,54 @@
             <div class="row align-items-center row-login">
             <div class="col-lg-6 text-center">
                 <img
-                src="images/login-image.png"
-                alt="login-image"
-                class="w-50 mb-4 mb-lg-none"
+                    src="images/login-image.png"
+                    alt="login-image"
+                    class="w-50 mb-4 mb-lg-none"
                 />
             </div>
             <div class="col-lg-5">
                 <h2>
-                Belanja kebutuhan utama, <br />
-                menjadi lebih mudah
+                    Belanja kebutuhan utama, <br />
+                    menjadi lebih mudah
                 </h2>
-                <form action="" class="mt-3">
-                <div class="form-group">
-                    <label for="emailAddress">Email Address</label>
-                    <input
-                    type="email"
-                    class="form-control w-75"
-                    id="emailAddress"
-                    value="Email Address"
-                    />
-                </div>
-                <div class="form-group mt-3">
-                    <label for="password">Password</label>
-                    <input
-                    type="password"
-                    class="form-control w-75"
-                    id="password"
-                    value="password"
-                    />
-                </div>
-                <a
-                    href="dashboard.html"
-                    class="btn btn-success btn-block w-75 mt-4"
-                >
-                    Sign In to My Account
-                </a>
-                <a
-                    href="register.html"
-                    class="btn btn-signup btn-block w-75 mt-2"
-                >
-                    Sign Up
-                </a>
+                <form method="POST" action="{{ route('login') }}" class="mt-3">
+                    @csrf
+                    {{-- Input Email --}}
+                    <div class="form-group">
+                        <label for="emailAddress">Email Address</label>
+                        <input id="emailAddress" type="email" class="form-control w-75 @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+                        @error('email')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+
+                    {{-- Input Password --}}
+                    <div class="form-group mt-3">
+                        <label for="password">Password</label>
+                        <input id="password" type="password" class="form-control w-75 @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" value="Input Password">
+
+                        @error('password')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+                    <button
+                        type="submit"
+                        href="dashboard.html"
+                        class="btn btn-success btn-block w-75 mt-4"
+                    >
+                        Sign In to My Account
+                    </button>
+                    <a
+                        href="{{ route('register') }}"
+                        class="btn btn-signup btn-block w-75 mt-2"
+                    >
+                        Sign Up
+                    </a>
                 </form>
             </div>
             </div>
--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -35,18 +35,81 @@
         <li class="nav-item">
           <a class="nav-link" href="#">Rewards</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="{{ route('register') }}">Sign Up</a>
-        </li>
-        <li class="nav-item">
-          <a
-            class="btn btn-success nav-link px-4 text-white"
-            href="{{ route('login') }}"
-          >
-            Sign In
-          </a>
-        </li>
+        @guest
+          <li class="nav-item">
+            <a class="nav-link" href="{{ route('register') }}">
+              Sign Up
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a
+              class="btn btn-success nav-link px-4 text-white"
+              href="{{ route('login') }}"
+            >
+              Sign In
+            </a>
+          </li>
+        @endguest
       </ul>
+
+      @auth
+        <ul class="navbar-nav d-none d-lg-flex">
+          <li class="nav-item dropdown">
+            <a
+              href="#"
+              class="nav-link"
+              id="navbarDropdown"
+              role="button"
+              data-toggle="dropdown"
+            >
+              <img
+                src="images/user_pc.png"
+                alt="user-pic"
+                class="rounded-circle mr-2 profile-picture"
+              />
+              Hi, {{ Auth::user()->name }}
+            </a>
+            <div class="dropdown-menu">
+              <a href="{{ route('dashboard') }}" class="dropdown-item">
+                Dashboard
+              </a>
+              <a href="{{ route('dashboard-settings-account') }}" class="dropdown-item">
+                Settings
+              </a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" href="{{ route('logout') }}"
+                  onclick="event.preventDefault();
+                    document.getElementById('logout-form').submit();">
+                  {{ __('Logout') }}
+              </a>
+
+              <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                @csrf
+              </form>
+            </div>
+          </li>
+          <!-- Icon Chart -->
+          <li class="nav-item">
+            <a href="#" class="nav-link d-inline-block mt-2">
+              <img src="icon/ic_cart.svg" alt="ic_cart_empty" />
+            </a>
+          </li>
+        </ul>
+
+        <!-- Mobile Menu -->
+        <ul class="navbar-nav d-block d-lg-none">
+          <li class="nav-item">
+            <a href="#" class="nav-link">Hi, Mandalika</a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link d-inline-block">Cart</a>
+          </li>
+          <li class="nav-item">
+            <a href="/" class="nav-link d-inline-block">Logout</a>
+          </li>
+        </ul>
+      @endauth
     </div>
   </div>
 </nav>
